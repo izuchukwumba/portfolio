2ad519a4a88022f7994db61cb01b7cb5299dfcc4
Izuchukwu Mba <izuchukwumba100@gmail.com>
2023-09-17 03:57:06 -0500

    w

--- src/Main.js
+++ src/Main.js
@@ -7,6 +7,7 @@ import NodeJS from "./images/nodejs_logo.png"
 import NodeJS2 from "./images/nodejs.png"
 import ReactLogo from "./images/react-logo.png"
 import Typescript from "./images/typescript.png"
+import PythonLogo from "./images/python-logo.png"
 
 export default function Main(){
 
@@ -20,11 +21,12 @@ export default function Main(){
 
             <div class = "regular-text">I build highly functional web application using:</div>
             <div className = "tech-logo-container">
-                <img className = "tech-logo" src = {jsImage} alt = "js"/>
-                <img className = "tech-logo" src = {MongoDB} alt = "mongo"/>
-                <img className = "tech-logo" src = {NodeJS} alt = "node"/>
-                <img className = "tech-logo" src = {ReactLogo} alt = "react"/>
-                <img className = "tech-logo" src = {Typescript} alt = "type"/>
+                <img className = "tech-logo js" src = {jsImage} alt = "js"/>
+                <img className = "tech-logo ts" src = {Typescript} alt = "type"/>
+                <img className = "tech-logo react" src = {ReactLogo} alt = "react"/>
+                <img className = "tech-logo mongo" src = {MongoDB} alt = "mongo"/>
+                <img className = "tech-logo node" src = {NodeJS} alt = "node"/>
+                <img className = "tech-logo python" src = {PythonLogo} alt = "python"/>
             </div>
             
             
