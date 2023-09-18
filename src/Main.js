31ab869b679844718e0e92f3541c7f15525f90f9
Izuchukwu Mba <izuchukwumba100@gmail.com>
2023-09-18 06:09:32 -0500

    ii

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,28 @@
 import "./styles/Main.css";
+import techText from "./data/techText.js";
+import { useState, useEffect } from "react";
+
 
 export default function Main(){
+    //   let [text, setText] = useState("");
+    //   let [isTextFullySpelled, setIsTextFullySpelled] = useState(false);
+    //     let targetText = ""
+    //   useEffect(() => {
 
-
+    //     let currentIndex = 0;
+    
+    //     const interval = setInterval(() => {
+    //       if (currentIndex === targetText.length) {
+    //         clearInterval(interval);
+    //         setIsTextFullySpelled(true); // Set the flag to true when the text is fully spelled
+    //       } else {
+    //         setText(targetText.substring(0, currentIndex + 1));
+    //         currentIndex++;
+    //       }
+    //     }, 100); // Adjust the interval as needed
+    
+    //     return () => clearInterval(interval);
+    //   }, []);    
     
     return( 
         <div className="main">
@@ -15,19 +35,9 @@ export default function Main(){
             <div class = "regular-text">I build highly functional web application using:</div>
             
             <div className = "typewriter">
-                <div className="typewritter-content">
-                    <span>HTML&nbsp;&nbsp;&nbsp;</span>
-                    <span>TailwindCSS&nbsp;&nbsp;&nbsp;</span>
-                    <span>JavaScript&nbsp;&nbsp;&nbsp;</span>
-                    <span>Python&nbsp;&nbsp;&nbsp;</span>
-                    <span>React&nbsp;&nbsp;&nbsp;</span>
-                    <span>Swift&nbsp;&nbsp;&nbsp;</span>
-                    <span>NodeJS&nbsp;&nbsp;&nbsp;</span>
-                    <span>TypeScript&nbsp;&nbsp;&nbsp;</span>
-                    <span>MongoDB&nbsp;&nbsp;&nbsp;</span>
-                    <span>ExpressJS&nbsp;&nbsp;&nbsp;</span>
-                </div>
+                {techText.map((item, index) => (<div key = {index}> {item}</div>))}
+                
             </div>
         </div>
-    )
+    ) ;
 }
---
+++ src/data/techText.js
@@ -0,0 +1,14 @@
+const techText = [
+    'HTML',
+    'TailwindCSS',
+    'JavaScript',
+    'Python',
+    'React',
+    'Swift',
+    'NodeJS',
+    'TypeScript',
+    'MongoDB',
+    'ExpressJS'
+]
+
+export default techText;
