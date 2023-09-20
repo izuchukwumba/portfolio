539b3941e5185f53781b665dc3afae15e3f64bf6
Izuchukwu Mba <izuchukwumba100@gmail.com>
2023-09-20 01:42:51 -0500

    newBatch

--- src/Footer.js
+++ src/Footer.js
@@ -80,25 +80,28 @@ export default function Footer(){
 
 
     return(
-        <div className = "footer">
-
-            <div className="contact-footer twitter" >
-            <a target = "_blank" href = "https://twitter.com/the-izuchukwu">
-                <i className="fa-brands fa-twitter"  style={twitterIconStyle}  onMouseEnter={handleMouseEnterTwitter} onMouseLeave={handleMouseLeaveTwitter}></i>
-            </a>
-            </div>
-
-            <div className="contact-footer github">
-                <a target = "_blank" href="https://github.com/izuchukwumba">
-                    <i className="fa-brands fa-github" style={githubIconStyle}  onMouseEnter={handleMouseEnterGithub} onMouseLeave={handleMouseLeaveGithub}></i>
-                </a>
-            </div>
-
-            <div className="contact-footer email">
-                <a href = "mailto:izuchukwumba100@gmail.com">
-                <i className="fa-solid fa-envelope" style={emailIconStyle}  onMouseEnter={handleMouseEnterEmail} onMouseLeave={handleMouseLeaveEmail}></i>
-                </a> 
+        <div id = "footer">
+            <div>Contact me on:</div>
+            <div className="contact-container">
+                <div className="contact-footer twitter" >
+                    <a target = "_blank" href = "https://twitter.com/the-izuchukwu">
+                        <i className="fa-brands fa-twitter"  style={twitterIconStyle}  onMouseEnter={handleMouseEnterTwitter} onMouseLeave={handleMouseLeaveTwitter}></i>
+                    </a>
                 </div>
+
+                <div className="contact-footer github">
+                    <a target = "_blank" href="https://github.com/izuchukwumba">
+                        <i className="fa-brands fa-github" style={githubIconStyle}  onMouseEnter={handleMouseEnterGithub} onMouseLeave={handleMouseLeaveGithub}></i>
+                    </a>
+                </div>
+
+                <div className="contact-footer email">
+                    <a href = "mailto:izuchukwumba100@gmail.com">
+                        <i className="fa-solid fa-envelope" style={emailIconStyle}  onMouseEnter={handleMouseEnterEmail} onMouseLeave={handleMouseLeaveEmail}></i>
+                    </a> 
+                </div>
+            </div>
+            <div className = "copyright">2023 &copy; IZUCHUKWU MBA</div>
         </div>
     )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,37 @@ import cuteDog from "./images/cute-dog.gif"
 
 export default function Navbar(){
 
+
 let [isShown, setIsShown] = useState(false)
+
+let modalStyle = {
+    opacity: isShown ? 1 : 0,
+    transition: "opacity 0.7s ease",
+    backgroundColor: "#27AE60",
+    color:" #171717",
+    width: "15.625rem",
+    height: "4.0625rem",
+    textAlign: "center",
+    padding: "0.625rem",
+    borderRadius:" 0.625rem",
+    position: "absolute",
+    right: "20%",
+    top: "7%",
+    cursor: "pointer",
+    backgroundColor: "#27AE60",
+    color: "#171717",
+    width: "15.625rem",
+    height: "4.0625rem",
+    textAlign: "center",
+    padding: "0.625rem",
+    borderRadius: "0.625rem",
+    position: "absolute",
+    right: "20%",
+    top: "7%",
+    cursor: "pointer",
+}
+
+
 
     return (
         <div className = "nav-container">
@@ -14,18 +44,18 @@ let [isShown, setIsShown] = useState(false)
                 </div>
                 
                 <div className="nav-texts">
-                    <div className = "resume nav-item">Resume</div>
+                    <div className = "resume nav-item" onClick = {()=>window.alert("Resume is downloading...")}><a href = "/src/data/Resume_Izuchukwu_Mba.pdf">Resume</a></div>
                     <div className = "projects nav-item" onClick = {() => setIsShown(prevState => !prevState)}>Projects</div>
-                    <div className = "contact nav-item">Contact</div>
+                    <div className = "contact nav-item"><a href = "#footer">Contact</a></div>
                 </div>
             </div>
-            {isShown && (
-            <div className = "modal-projects modal" onClick = {() => setIsShown(false)}>
+        
+            <div className = "modal-projects modal" style = {modalStyle} onClick = {() => setIsShown(false)}>
                 <span className = "modal-text">Coming soon...</span>
                 <span className = "x">x</span>
                 
             </div>
-                )}
+                
 
 
 
